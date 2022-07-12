34d0dd213c94637522e331611d67afdd7aca2501
Aleksandra Ujvari <ujvari65@gmail.com>
2022-07-12 22:49:34 +0200

    Delete reverseString.test.js

--- reverseString.test.js
+++
@@ -1,7 +0,0 @@
-import reverseString from './reverse-string';
-
-const testingValue = 'hello'
-
-test(`Should be true when you pass ${testingValue}`, () => {
-  expect(reverseString(testingValue)).toMatch(/olleh/);
-});
