b41bb348c3bc1a1a4ec5045caf9ed5bd993b012c
Aleksandra Ujvari <ujvari65@gmail.com>
2022-07-12 22:49:08 +0200

    Delete capitalize.test.js

--- capitalize.test.js
+++
@@ -1,7 +0,0 @@
-import capitalize from './modules/capitalize.js';
-
-test('first letter should return capitalized', () => {
-  const string = 'hello';
-  const result = string[0].toUpperCase();
-  expect(result).toBe('H');
-});
